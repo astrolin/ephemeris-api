de448c89e5d2e4d60f40cdd4e5bac534c9a3f003
Orlin M Bozhinov <om@soundsapiens.com>
2017-01-21 18:34:01 +0200

    make project.clj ready for development

--- project.clj
+++ project.clj
@@ -1,12 +1,25 @@
 (defproject ephemeris-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Ephemeris HTTP API"
+  :min-lein-version  "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ephemeris "0.0.1-SNAPSHOT"]
                  [metosin/compojure-api "1.1.10"]]
   :ring {:handler ephemeris-api.handler/app}
+  :profiles
+    {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
+                          [cheshire "5.5.0"]
+                          [ring/ring-mock "0.3.0"]
+                          [proto-repl "0.3.1"]
+                          [midje "1.8.3"]
+                          [midje-notifier "0.2.0"]]
+           :plugins [[lein-ring "0.10.0"]
+                     [lein-midje "3.2"]]
+           :repl-options {:init-ns ephemeris-api.handler}}
+     :repl
+       {:ultra {:repl {:sort-keys false
+                       :map-coll-separator :line}}}}
+  :aliases {"test" ["midje"]
+            "autotest" ["midje" ":autotest"]}
+  :pom-location "target/"
   :uberjar-name "server.jar"
-  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
-                                  [cheshire "5.5.0"]
-                                  [ring/ring-mock "0.3.0"]
-                                  [midje "1.8.3"]]
-                   :plugins [[lein-ring "0.10.0"]
-                             [lein-midje "3.2"]]}})
+  :deploy-branches ["master"])
