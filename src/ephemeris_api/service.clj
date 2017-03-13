ea76ee486ce303c626bbb34acb598d786da4d500
Orlin M Bozhinov <om@soundsapiens.com>
2017-03-13 13:16:55 +0200

    simplify the api service structure

--- src/ephemeris_api/service.clj
+++ src/ephemeris_api/service.clj
@@ -22,25 +22,22 @@
                    :houses false
                    :meta false})})))
 
-;;(s/with-fn-validation) ;; Optional, but nice to have at compile time
+;; (s/with-fn-validation) ;; Optional, but nice to have at compile time
 (api/defroutes routes
   {:info {:title       "Ephemeris Api"
           :description "For Astrology Applications"
-          :version     "0.1"}
-   :tags [{:name         "ephemeris"
-           :description  "calculate"
-           :externalDocs {:description "Find out more"
-                          :url         "https://github.com/astrolet/ephemeris-api"}}]}
-  [[["/" ^:interceptors [api/error-responses
+          :version     "0.1"
+          :externalDocs {:description "Find out more"
+                         :url         "https://github.com/astrolet/ephemeris-api"}}}
+  [[["/api" ^:interceptors [api/error-responses]
                          (api/negotiate-response)
                          (api/body-params)
                          api/common-body
                          (api/coerce-request)
-                         (api/validate-response)]
-     ["/api" ^:interceptors [(api/doc {:tags ["ephemeris"]})]
-      ["/utc" {:get mundane}]]]
-    ["/swagger.json" {:get api/swagger-json}]
-    ["/*resource" {:get api/swagger-ui}]]])
+                         (api/validate-response)
+      ["/utc" {:get mundane}]
+      ["/swagger.json" {:get api/swagger-json}]
+      ["/*resource" {:get api/swagger-ui}]]]])
 
 (def service
   {:env :prod
