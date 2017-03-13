bdb4610226b4729e3c7a6f8afb338ce728695975
Orlin M Bozhinov <om@soundsapiens.com>
2017-03-13 16:25:50 +0200

    parinfer indentation fix of api service

--- src/ephemeris_api/service.clj
+++ src/ephemeris_api/service.clj
@@ -1,6 +1,5 @@
 (ns ephemeris-api.service
   (:require [io.pedestal.http :as bootstrap]
-            [io.pedestal.interceptor :refer [interceptor]]
             [io.pedestal.interceptor.helpers :refer [before handler]]
             [pedestal-api
              [core :as api]]
@@ -30,12 +29,13 @@
           :version     "0.1"
           :externalDocs {:description "Find out more"
                          :url         "https://github.com/astrolet/ephemeris-api"}}}
-  [[["/api" ^:interceptors [api/error-responses]
-                         (api/negotiate-response)
-                         (api/body-params)
-                         api/common-body
-                         (api/coerce-request)
-                         (api/validate-response)
+  [[["/api" ^:interceptors
+            [api/error-responses
+             (api/negotiate-response)
+             (api/body-params)
+             api/common-body
+             (api/coerce-request)
+             (api/validate-response)]
       ["/utc" {:get mundane}]
       ["/swagger.json" {:get api/swagger-json}]
       ["/*resource" {:get api/swagger-ui}]]]])
