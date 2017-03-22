2ca3f3c69091566bee11ff4961b5b8990dd0112c
Orlin M Bozhinov <om@soundsapiens.com>
2017-03-22 17:54:00 +0200

    trying to figure out how it works; wip...

--- test/ephemeris_api/core_test.clj
+++ test/ephemeris_api/core_test.clj
@@ -1,6 +1,11 @@
 (ns ephemeris-api.core-test
-  (:require [midje.sweet :refer :all]))
+  (:require [midje.sweet :refer :all]
+            [martian.core :as martian]
+            [martian.test :as martian-test]))
 
-(facts "Core"
-  (fact "TODO: test"
-    1 => 1))
+(facts "API"
+  (let [m (martian/bootstrap-swagger
+            "http://api.astrolin.org/swagger.json"
+            {:interceptors [martian-test/generate-response]})]
+    (fact "now"
+      (martian/response-for m :mundane) => nil)))
